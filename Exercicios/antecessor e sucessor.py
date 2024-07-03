d4c57078d32c1b86f1da71f3abad64f6a5ce9128
Mik3532 <miqueias.alves3532@gmail.com>
2024-07-03 19:53:10 -0300

    exe
    Atividade de fixação

---
+++ Exercicios/antecessor e sucessor.py
@@ -0,0 +1,14 @@
+#nesse Programa o usuario vai digitar um numero e o programa vai 
+#mostra o antecessor e o sucessor
+numero = input("Digite um numero:")
+
+numero =  int(numero)
+
+antecessor = numero -1
+sucessor =  numero +1
+
+print (f"O numero que você digitou e {numero}, o Antecessor dele é {antecessor} e o Sucessor é {sucessor}")
+
+
+
+
